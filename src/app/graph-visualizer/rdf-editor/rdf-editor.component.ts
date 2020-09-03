88db1667a0c74647981be9c1209b949f44413d53
Stefan@Lap <stefan.muennich@unibas.ch>
2020-09-03 17:53:07 +0200

    feat(graph): use types for rdf-js

--- src/app/graph-visualizer/rdf-editor/rdf-editor.component.ts
+++ src/app/graph-visualizer/rdf-editor/rdf-editor.component.ts
@@ -1,9 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 
-import * as N3 from 'n3';
+import { EditorConfiguration } from 'codemirror';
+import 'codemirror/mode/turtle/turtle';
+import 'codemirror/addon/edit/matchbrackets';
 
-const { DataFactory } = N3 ;
-const { namedNode, literal, defaultGraph } = DataFactory;
+import * as N3 from 'n3';
 
 @Component({
     selector: 'app-rdf-editor',
@@ -11,52 +12,54 @@ const { namedNode, literal, defaultGraph } = DataFactory;
     styleUrls: ['./rdf-editor.component.css']
 })
 export class RdfEditorComponent implements OnInit {
-
     /**
      * Public variable: cmTriplesConfig.
      *
      * It keeps the Codemirror configuration for the triples panel.
      */
-    cmTriplesConfig = {
+    cmTriplesConfig: EditorConfiguration = {
+        mode: 'turtle',
         lineNumbers: true,
         firstLineNumber: 1,
         lineWrapping: true,
-        matchBrackets: true,
-        mode: 'turtle'
+        matchBrackets: true
     };
 
-    triples = `PREFIX c: <http://example.org/cartoons#>
+    initialTriples = `@prefix c: <http://example.org/cartoons#> .
 
-            c:Tom a c:Cat.
-            c:Jerry a c:Mouse;
-                    c:smarterThan c:Tom.`;
+        c:Tom a c:Cat .
+        c:Jerry a c:Mouse ;
+                c:smarterThan c:Tom .`;
 
-    parser = new N3.Parser();
-    store = new N3.Store();
-    writer = new N3.Writer();
+    triples: string;
+
+    parser: N3.Parser;
+    store: N3.Store;
+    writer: N3.Writer;
 
     ngOnInit(): void {
-        this.parseTriples();
+        this.setInitialTriples();
     }
 
+    parseTriples(triples: string): void {
+        this.parser = new N3.Parser();
+        this.store = new N3.Store();
+        this.writer = new N3.Writer();
 
-    parseTriples(): void {
-        this.parser.parse(this.triples, (error, quad, prefixes) => {
-                if (quad) {
-                    console.log(quad);
-                    this.store.addQuad(quad);
-                }
-                else {
-                    console.log('# That\'s all, folks! ', prefixes);
-                    this.writer.addPrefixes(prefixes);
-                    this.writeTriples();
-
-                }
-            });
+        this.parser.parse(triples, (error: Error, quadValue: N3.Quad, prefixes: N3.Prefixes) => {
+            if (quadValue) {
+                console.log(quadValue);
+                this.store.addQuad(quadValue);
+            } else {
+                console.log('That is all, folks!', prefixes);
+                this.writer.addPrefixes(prefixes);
+                this.writeTriples();
+            }
+        });
     }
 
     writeTriples(): void {
-        const quads = this.store.getQuads(null, null, null);
+        const quads = this.store.getQuads(null, null, null, N3.DataFactory.defaultGraph());
         console.log('quads:', quads);
         this.writer.addQuads(quads);
         this.writer.end((e, result) => {
@@ -67,4 +70,15 @@ export class RdfEditorComponent implements OnInit {
         });
     }
 
+    setInitialTriples(): void {
+        if (!this.initialTriples) {
+            return;
+        }
+        this.triples = this.initialTriples;
+        this.updateTriples();
+    }
+
+    updateTriples(): void {
+        this.parseTriples(this.triples);
+    }
 }
