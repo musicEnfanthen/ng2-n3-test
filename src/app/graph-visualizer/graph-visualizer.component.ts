6fccf05c870e35790bd3d51504c5daf09dfa001e
Stefan@Lap <stefan.muennich@unibas.ch>
2020-09-03 17:54:41 +0200

    feat(graph): add sparql editor

--- src/app/graph-visualizer/graph-visualizer.component.ts
+++ src/app/graph-visualizer/graph-visualizer.component.ts
@@ -1,12 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-graph-visualizer',
-  templateUrl: './graph-visualizer.component.html',
-  styleUrls: ['./graph-visualizer.component.css']
+    selector: 'app-graph-visualizer',
+    templateUrl: './graph-visualizer.component.html',
+    styleUrls: ['./graph-visualizer.component.css']
 })
 export class GraphVisualizerComponent implements OnInit {
-
-
-
+    ngOnInit(): void {}
 }
--- src/app/graph-visualizer/sparql-editor/sparql-editor.component.ts
+++ src/app/graph-visualizer/sparql-editor/sparql-editor.component.ts
@@ -1,15 +1,60 @@
 import { Component, OnInit } from '@angular/core';
 
+import { EditorConfiguration } from 'codemirror';
+import 'codemirror/addon/edit/matchbrackets';
+import 'codemirror/mode/sparql/sparql';
+
 @Component({
-  selector: 'app-sparql-editor',
-  templateUrl: './sparql-editor.component.html',
-  styleUrls: ['./sparql-editor.component.css']
+    selector: 'app-sparql-editor',
+    templateUrl: './sparql-editor.component.html',
+    styleUrls: ['./sparql-editor.component.css']
 })
 export class SparqlEditorComponent implements OnInit {
+    /**
+     * Public variable: cmSparqlConfig.
+     *
+     * It keeps the Codemirror configuration for the sparql panel.
+     */
+    cmSparqlConfig: EditorConfiguration = {
+        mode: 'sparql',
+        lineNumbers: true,
+        firstLineNumber: 1,
+        lineWrapping: true,
+        matchBrackets: true
+    };
 
-  constructor() { }
+    initialQuery = `PREFIX c: <http://example.org/cartoons#>
 
-  ngOnInit(): void {
-  }
+        SELECT DISTINCT *
+        WHERE {
+            ?s ?p ?o .
+        }
+        LIMIT 50
+    `;
 
+    query: any;
+
+    ngOnInit(): void {
+        this.setInitialQuery();
+    }
+
+    /**
+     * Public method: setInitialQuery.
+     *
+     * It sets the initial value of the query variable
+     * from the RDF input data.
+     *
+     * @returns {void} Sets the initial query.
+     */
+    setInitialQuery(): void {
+        if (!this.initialQuery) {
+            return;
+        }
+        this.query = this.initialQuery;
+        this.doQuery();
+    }
+
+    doQuery(): void {
+        console.log('query', this.query);
+    }
 }
